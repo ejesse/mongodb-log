9d857f016e3d5625041658721b39ffeddaaa3c95
Jesse Emery <jesse@onagertech.com>
2011-07-30 19:28:00 -0400

    moved everything to init instead of to

--- mongolog/handlers.py
+++ mongolog/handlers.py
@@ -13,27 +13,22 @@ class MongoFormatter(logging.Formatter):
         return data
     
 
-class MongoHandler(logging.Handler,Connection):
+class MongoHandler(logging.Handler):
     """ Custom log handler
 
     Logs all messages to a mongo collection. This  handler is 
     designed to be used with the standard python logging mechanism.
     """
 
-    @classmethod
-    def to(cls, db, collection, host='localhost', port=None, level=logging.NOTSET):
-        """ Create a handler for a given  """
-        return cls(Connection(host, port)[db][collection])
-        
-    def __init__(self, host='127.0.0.1', port=27017, db, collection, level=logging.NOTSET):
+    def __init__(self,  db, collection, host='127.0.0.1', port=27017,level=logging.NOTSET):
         """ Init log handler and store the collection handle """
         logging.Handler.__init__(self, level)
-        Connection.__init__((host, port)[db][collection])
-        self.collection = collection
+        self.connection = Connection.__init__((host, port))
         self.formatter = MongoFormatter()
         self.host = host
         self.port = port
-        self.db = db
+        self.db = self.connection[db]
+        self.collection = db[collection]
 
     def emit(self,record):
         """ Store the record to the collection. Async insert """
